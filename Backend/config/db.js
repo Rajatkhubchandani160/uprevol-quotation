b236e18d11d2c2ba0f81aff12c77fcc274d34f99
RajatKhubchandani160 <rajatkhubchndani16@gmaill.com>
2024-07-03 02:48:06 +0530

    updated databses URI configuration

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose');
 async function connectDB(){
     try{
         // await mongoose.connect("mongodb://127.0.0.1:27017/Uprevol")
-        await mongoose.connect('mongodb+srv://Rajatkhubchandani:rajat@uprevol.kthyvep.mongodb.net/')
+        await mongoose.connect('mongodb+srv://Rajatkhubchandani:rajat@uprevol.kthyvep.mongodb.net/Uprevol')
+        
        
     }catch(err){
         console.log(err)
